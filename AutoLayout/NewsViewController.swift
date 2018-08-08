2d4df39a9a3459930a2c093838c67aeaee001677
peterng014 <phuhitec@gmail.com>
2018-08-08 23:05:08 +0700

    Add files via upload
    add more

---
+++ AutoLayout/NewsViewController.swift
@@ -0,0 +1,35 @@
+//
+//  NewsViewController.swift
+//  AutoLayout
+//
+//  Created by Nguyen Phu on 8/8/18.
+//  Copyright © 2018 Nguyen Phu. All rights reserved.
+//
+
+import UIKit
+
+class NewsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
